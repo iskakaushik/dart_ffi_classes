d8605bce331219a65ecd6d93641f4ccb0d85577d
Kaushik Iska <kaushikiska@google.com>
2021-07-07 15:15:36 +0000

    Things look good son!

--- main.dart
+++ main.dart
@@ -1,3 +1,27 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+import 'generated_bindings.dart';
+
+String _getDylibPath() {
+  final rootDir = Directory.current.absolute.path;
+  var path = p.join(rootDir, 'simple_cpp');
+  if (Platform.isMacOS) {
+    path = p.join(path, 'libsimple-cpp-lib.dylib');
+  } else {
+    path = p.join(path, 'libsimple-cpp-lib.so');
+  }
+  return path;
+}
+
 void main() {
   print('Hello, World!');
+  final int a = 50;
+  final int b = 43;
+
+  final dylib = DynamicLibrary.open(_getDylibPath());
+  final int sum = NativeLibrary(dylib).simple_add(a, b);
+  print('Sum of $a and $b is $sum.');
 }
